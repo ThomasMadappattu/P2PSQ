71759a0866026f14ea5952b9b1e53fe91f55873a
ThomasMadappattu <sandeep.sandos@gmail.com>
2015-06-22 16:04:07 -0700

    Rudimentry DSR routing

--- src/com/d2dsq/routing/SenderInterface.java
+++ src/com/d2dsq/routing/SenderInterface.java
@@ -1,8 +1,10 @@
 package com.d2dsq.routing;
-
+import com.d2dsq.models.*;
 public interface SenderInterface
 {
 
-	public void SendPackect(); 
+	public void SendPackect(String mDeviceName, Packet packet); 
+	public void SendMessage(String mDeviceNam, Message message);
+	public String GetPeers();
 	
 }
